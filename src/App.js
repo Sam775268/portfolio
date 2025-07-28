c45e745a8c5a21491c2c158d7796fa44181379b1
Sam <sam@mac.home>
2025-07-28 18:47:06 -0400

    Fix navbar not showing on vercel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
+import About from './pages/About'; 
+import Projects from './pages/Projects'; 
+import Services from './pages/Services'; 
 import Contact from './pages/Contact';
-import Services from './pages/Services';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <div>
+      <nav>
+  <NavLink to="/" style={{ marginRight: '10px' }}>Home</NavLink>
+  <NavLink to="/about" style={{ marginRight: '10px' }}>About</NavLink>
+  <NavLink to="/projects" style={{ marginRight: '10px' }}>Projects</NavLink>
+  <NavLink to="/services" style={{ marginRight: '10px' }}>Services</NavLink>
+  <NavLink to="/contact" style={{ marginRight: '10px' }}>Contact</NavLink>
+</nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
